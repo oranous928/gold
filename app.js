29e73789195013e6a2082da8e136655ce3606078
Behrooz Shahin <shahin.behrooz@gmail.com>
2025-02-28 04:07:48 -0500

    freq

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const host = '0.0.0.0';
 server.listen(port,host);
 server.get('/',(req,res)=>res.send('Hello'));
 server.get('/:state',(req,res)=>stateHandler(req,res));
-
 function stateHandler(req,res){
     res.send(req.params.state);
 }
