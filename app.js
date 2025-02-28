64fc078d1da7e569a7b65900ae148e88a669036b
Behrooz Shahin <shahin.behrooz@gmail.com>
2025-02-28 03:46:28 -0500

    trigger

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const host = '0.0.0.0';
 server.listen(port,host);
 server.get('/',(req,res)=>res.send('Hello'));
 server.get('/:state',(req,res)=>stateHandler(req,res));
+
 function stateHandler(req,res){
     res.send(req.params.state);
 }
